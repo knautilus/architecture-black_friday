db30a6e5e3aa7a3879fa83096fbee6a22096ba4b
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-08-20 02:49:20 +0300

    Task 2

---
+++ mongo-sharding/scripts/mongo-setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+echo Инициализация сервера конфигурации
+
+until mongosh --host configSrv:27017 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done 
+mongosh --host configSrv:27017 --eval '
+rs.initiate(
+  {
+    _id : "config_server",
+    configsvr: true,
+    members: [
+      { _id : 0, host : "configSrv:27017" }
+    ]
+  }
+);'
+
+echo Инициализация Shard 1
+
+until mongosh --host shard1:27018 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+mongosh --host shard1:27018 --eval "
+    rs.initiate(
+      {
+        _id : 'shard1',
+        members: [
+          { _id : 0, host : 'shard1:27018' },
+        ]
+      }
+    );
+"
+
+echo Инициализация Shard 2
+
+until mongosh --host shard2:27019 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+mongosh --host shard2:27019 --eval "
+    rs.initiate(
+      {
+        _id : 'shard2',
+        members: [
+          { _id : 1, host : 'shard2:27019' },
+        ]
+      }
+    );
+"
+
+echo Инициализация роутера и заливка данных
+
+until mongosh --host mongos_router:27020 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+
+mongosh --host mongos_router:27020 --eval "
+    sh.addShard('shard1/shard1:27018');
+    sh.addShard('shard2/shard2:27019');
+    sh.enableSharding('somedb');
+    sh.shardCollection('somedb.helloDoc', { 'name' : 'hashed' } );
+"
+
+mongosh --host mongos_router:27020 --eval "
+    db = db.getSiblingDB('somedb');
+    for(var i = 0; i < 1000; i++)
+    {
+      db.helloDoc.insertOne({age:i, name:'ly'+i});
+    }
+    db.helloDoc.getShardDistribution()
+    print('Total:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard1:27018 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard1:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard2:27019 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard2:', db.helloDoc.countDocuments())
+"
