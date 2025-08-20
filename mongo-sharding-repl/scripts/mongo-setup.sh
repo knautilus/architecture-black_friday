8be5ccddd8dd2fdbfeec5b27dd3d9d38e1b86b76
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-08-21 01:29:01 +0300

    Task 3 - Update addresses

--- mongo-sharding-repl/scripts/mongo-setup.sh
+++ mongo-sharding-repl/scripts/mongo-setup.sh
@@ -16,13 +16,17 @@ rs.initiate(
 
 echo Инициализация Shard 1
 
-until mongosh --host shard1:27018 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
-mongosh --host shard1:27018 --eval "
+until mongosh --host shard1_r1:27018 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+until mongosh --host shard1_r2:27019 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+until mongosh --host shard1_r3:27020 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+mongosh --host shard1_r1:27018 --eval "
     rs.initiate(
       {
         _id : 'shard1',
         members: [
-          { _id : 0, host : 'shard1:27018' },
+          { _id : 0, host : 'shard1_r1:27018', priority : 3 },
+          { _id : 1, host : 'shard1_r2:27019', priority : 2 },
+          { _id : 2, host : 'shard1_r3:27020', priority : 1 }
         ]
       }
     );
@@ -30,13 +34,17 @@ mongosh --host shard1:27018 --eval "
 
 echo Инициализация Shard 2
 
-until mongosh --host shard2:27021 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
-mongosh --host shard2:27021 --eval "
+until mongosh --host shard2_r1:27021 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+until mongosh --host shard2_r2:27022 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+until mongosh --host shard2_r3:27023 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
+mongosh --host shard2_r1:27021 --eval "
     rs.initiate(
       {
         _id : 'shard2',
         members: [
-          { _id : 1, host : 'shard2:27021' },
+          { _id : 0, host : 'shard2_r1:27021', priority : 3 },
+          { _id : 1, host : 'shard2_r2:27022', priority : 2 },
+          { _id : 2, host : 'shard2_r3:27023', priority : 1 }
         ]
       }
     );
@@ -47,8 +55,8 @@ echo Инициализация роутера и заливка данных
 until mongosh --host mongos_router:27024 --eval 'db.adminCommand("ping")' | grep 'ok'; do sleep 2; done
 
 mongosh --host mongos_router:27024 --eval "
-    sh.addShard('shard1/shard1:27018');
-    sh.addShard('shard2/shard2:27021');
+    sh.addShard('shard1/shard1_r1:27018,shard1_r2:27019,shard1_r3:27020');
+    sh.addShard('shard2/shard2_r1:27021,shard2_r2:27022,shard2_r3:27023');
     sh.enableSharding('somedb');
     sh.shardCollection('somedb.helloDoc', { 'name' : 'hashed' } );
 "
@@ -62,12 +70,32 @@ mongosh --host mongos_router:27024 --eval "
     print('Total:', db.helloDoc.countDocuments())
 "
 
-mongosh --host shard1:27018 --eval "
+mongosh --host shard1_r1:27018 --eval "
     db = db.getSiblingDB('somedb');
-    print('Shard1:', db.helloDoc.countDocuments())
+    print('Shard1-1:', db.helloDoc.countDocuments())
 "
 
-mongosh --host shard2:27021 --eval "
+mongosh --host shard1_r2:27019 --eval "
     db = db.getSiblingDB('somedb');
-    print('Shard2:', db.helloDoc.countDocuments())
+    print('Shard1-2:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard1_r3:27020 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard1-3:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard2_r1:27021 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard2-1:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard2_r2:27022 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard2-2:', db.helloDoc.countDocuments())
+"
+
+mongosh --host shard2_r3:27023 --eval "
+    db = db.getSiblingDB('somedb');
+    print('Shard2-3:', db.helloDoc.countDocuments())
 "
